d65c6108dc880ddb2082816bb6902fef2c63283b
dret <erik.wilde@dret.net>
2025-02-10 13:31:37 -0800

    simplifying function

--- functions/no-content-on-204.js
+++ functions/no-content-on-204.js
@@ -2,9 +2,8 @@ module.exports = function (targetVal, opts, paths) {
   if (typeof targetVal === "object" && "content" in targetVal) {
     return [
       {
-        message: "Responses with status code 204 must not have a content property.",
-        path: [...(paths.target ?? []), "content"],
-      },
+        message: "Responses with status code 204 must not have a content property."
+      }
     ];
   }
   return [];
