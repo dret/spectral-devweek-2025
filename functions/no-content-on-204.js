077f1de9d9917ae678ed098b0bc4c664e0b3c4ca
dret <erik.wilde@dret.net>
2025-02-10 13:13:00 -0800

    adding content on 204 files

---
+++ functions/no-content-on-204.js
@@ -0,0 +1,11 @@
+module.exports = function (targetVal, opts, paths) {
+  if (typeof targetVal === "object" && "content" in targetVal) {
+    return [
+      {
+        message: "Responses with status code 204 must not have a content property.",
+        path: [...(paths.target ?? []), "content"],
+      },
+    ];
+  }
+  return [];
+};
